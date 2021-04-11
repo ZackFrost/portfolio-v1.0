178f5d51406d23fc0b81778af7208837552e8d23
ZackFrost <ianpaul789@gmail.com>
2021-04-11 21:01:11 +0530

    technology icons replaced

--- src/components/icon_title_widget.js
+++ src/components/icon_title_widget.js
@@ -5,7 +5,7 @@ const IconTitleWidget = ({ path, title, isShowTitle = true }) => {
     <div id="iconTitle">
       <div className="row">
         <div id="icon" style={{ textAlign: "center", paddingBottom: "30px" }}>
-          <img src={path} alt={path} height="60" width="60" />
+          <img src={path} height="60" width="60" />
         </div>
         {isShowTitle && (
           <div id="iconTitle" style={{ color: "white", textAlign: "center", fontWeight:700}}>
